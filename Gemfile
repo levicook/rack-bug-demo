7e49824efef917ffd2a23683f4c0bba0dea24ad0
Levi Cook <levicook@gmail.com>
2010-08-30 18:44:38 -0500

    rack-bug

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source 'http://rubygems.org'
 gem 'rails', '3.0.0.rc2'
 gem 'mysql2'
 
-
 gem 'compass', '>= 0.10.4'
 gem 'factory_girl'
 gem 'ffaker'
@@ -11,11 +10,14 @@ gem 'nifty-generators'
 gem 'placeholder'
 gem 'will_paginate', '~> 3.0.pre2'
 
-
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
 group :development, :test do
+
+  # XXX
+  gem 'rack-bug', :require => 'rack/bug', :git => 'http://github.com/brynary/rack-bug.git', :branch => 'rails3'
+
   gem 'annotate'
   gem 'database_cleaner', '>= 0.6.0.rc.2'
   gem 'rails3-generators'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 RackBugDemo::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
 
+  # HINT: rake secret
+  config.middleware.use "Rack::Bug",
+    :password   => "secret",
+    :secret_key => "351a55fc54347710691b92192969ea3de5758f2bdaf2d1d1403bd90d5f3f98798dd028970e7e787cec468c6c3b4e41fc29448df646f7e6fe6b53f058cd209078"
+
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
   # since you don't have to restart the webserver when you make code changes.
@@ -22,5 +27,6 @@ RackBugDemo::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
 end
 
