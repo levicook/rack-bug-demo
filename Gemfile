969cd996179c42b5ab449ae37a868c37b6733543
Levi Cook <levicook@gmail.com>
2010-08-30 06:57:18 -0500

    minimal rails app

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.0.rc2'
+gem 'mysql2'
+
+gem 'factory_girl'
+gem 'nifty-generators'
+
+# To use debugger
+# gem 'ruby-debug'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+# group :development, :test do
+#   gem 'webrat'
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackBugDemo::Application.config.secret_token = '0360d0a861ab9c9d235d2ffa8be70bb192b0946cda1d2600fb1c9ac2a75d577499f7c514f039925b1b520639f7c80c393c94b7eaa7dc088003371c6f7538f2a8'
